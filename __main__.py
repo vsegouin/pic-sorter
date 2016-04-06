f6c7695b8f74472c618c9ea36977bcc6790352f3
ocalik <vincent.segouin@hotmail.fr>
2016-04-06 22:28:46 +0200

    use explore root enhance file md5 calculation

--- __main__.py
+++ __main__.py
@@ -1,13 +1,40 @@
-from image.ExifReader import ExifReader
-from files.FileBrowser import FileBrowser
 from files.MD5Encoder import MD5Encoder
+import os
 import sys
 
 root_path = sys.argv[1]
 
-fileBrow = FileBrowser(root_path)
-fileBrow.list_file()
+# init database.txt in root_path
 md5enc = MD5Encoder(root_path)
-for file in fileBrow.m_list_file:
-    if not md5enc.is_file_already_present(file):
-        md5enc.add_hash_in_database(md5enc.hash_file(file),file)
+
+print('walk_dir = ' + root_path+"\n")
+
+# If your current working directory may change during script execution, it's recommended to
+# immediately convert program arguments to an absolute path. Then the variable root below will
+# be an absolute path as well. Example:
+# walk_dir = os.path.abspath(walk_dir)
+print('walk_dir (absolute) = ' + os.path.abspath(root_path)+"\n")
+total_file_number = 0;
+total_file_number_processed = 0;
+for root, subdirs, files in os.walk(root_path):
+    file_number = 0
+    file_processed = 0
+    print('-- current directory = ' + root + "\n")
+    if("@eaDir" in root):
+        print("it's a eadir folder continue\n")
+        continue
+
+    for filename in files:
+        file_number += 1
+        file_path = os.path.join(root, filename)
+        md5enc.init_file(file_path)
+        if not md5enc.is_file_already_present() and file_path != os.path.join(root_path, "database.txt"):
+            file_processed += 1
+            md5enc.add_hash_in_database()
+        else :
+            pass
+    total_file_number += file_number
+    total_file_number_processed += file_processed
+    print("there was " + repr(file_number) + " in " + repr(root) + " and "+repr(file_processed)+" has been processed\n")
+
+print("At the end there was "+repr(total_file_number)+" and "+repr(total_file_number_processed)+" processed\n\nEND PROGRAM \n\n")
--- files/MD5Encoder.py
+++ files/MD5Encoder.py
@@ -2,30 +2,40 @@ import os, hashlib
 
 
 class MD5Encoder:
-    m_database_path = None
+    m_database_path = ""
+    m_current_file = ""
+    m_hashed_value = ""
 
     def __init__(self,database_location):
         self.m_database_path = os.path.join(database_location, "database.txt")
         file = open(self.m_database_path, "a", 1)
         file.close()
 
-    def hash_file(self,file):
-        hash_md5 = hashlib.md5()
-        with open(file, "rb") as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(chunk)
-        return hash_md5.hexdigest()
+    def init_file(self, file_path):
+        self.m_current_file = file_path
+        self.m_hashed_value = self.hash_file(file_path)
 
-    def is_file_already_present(self,file):
-        hashed_file = self.hash_file(file)
+    def hash_file(self,file):
+        self.m_current_file = file
+        hash_md5 = hashlib.md5()
+        try:
+            with open(file, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(chunk)
+            return hash_md5.hexdigest()
+        except FileNotFoundError:
+            print(file+" Not Found continuing")
+        return ""
+
+    def is_file_already_present(self):
         f = open(self.m_database_path)
         contents = f.read()
         f.close()
-        return contents.count(hashed_file) > 0
+        print(self.m_current_file+" "+self.m_hashed_value)
+        return True if self.m_hashed_value != "" and contents.count(self.m_hashed_value) > 0 else False
 
-    def add_hash_in_database(self, hash, fileName):
+    def add_hash_in_database(self):
         file = open(self.m_database_path, "a", 1)
-        file.write(hash)
-        file.write(' ('+fileName+ ')')
+        file.write(self.m_hashed_value)
         file.write("\n\r")
         file.close()
