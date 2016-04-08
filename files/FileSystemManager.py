7099ab7719ce36029d5038b1bf39f3f26bc8c7b0
Vincent.Segouin <Vincent.Segouin@gmail.com>
2016-04-08 16:21:32 +0200

    reformat file

--- files/FileSystemManager.py
+++ files/FileSystemManager.py
@@ -4,7 +4,9 @@ import time
 
 from image.ExifReader import ExifReader
 
-months = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"]
+months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre",
+          "Decembre"]
+
 
 class FileSystemManager:
     root_path = ""
@@ -39,7 +41,7 @@ class FileSystemManager:
         return folder
 
     def move_file(self, file_directory, filename, dest_directory, dest_filename):
-        basename,ext = os.path.splitext(dest_filename)
+        basename, ext = os.path.splitext(dest_filename)
         dst_file = os.path.join(dest_directory, dest_filename)
         # rename if necessary
         count = 0
@@ -53,13 +55,14 @@ class FileSystemManager:
         basename, ext = os.path.splitext(filename)
         dest_name = self.extract_datetime(file_exif)
         if file_exif == {} or dest_name == "":
-            dest_name,dest_directory = self.detect_file_date(directory,filename)
+            dest_name, dest_directory = self.detect_file_date(directory, filename)
         else:
             match = re.search(r'(\d{4}):(\d{2}):(\d{2})', dest_name).groups()
             dest_directory = self.create_folder_if_not_exists(os.path.join(self.processed_folder, "regular",
-                                                                                  repr(match[0]).replace("\\", "").replace("'",""),
-                                                                                  months[int(match[1])-1]))
-        self.move_file(directory, filename, dest_directory, (dest_name+ext).replace(":", "-"))
+                                                                           repr(match[0]).replace("\\", "").replace("'",
+                                                                                                                    ""),
+                                                                           months[int(match[1]) - 1]))
+        self.move_file(directory, filename, dest_directory, (dest_name + ext).replace(":", "-"))
 
     def manage_non_image(self, directory, filename, file_type):
         if file_type == "video":
@@ -79,28 +82,34 @@ class FileSystemManager:
         self.move_file(directory, filename, dest_directory, filename)
 
     def copy_directory_structure(self, directory_to_copy, directory_destination):
-        new_directory = directory_to_copy.replace(self.root_path,"")
-        new_directory = os.path.join(directory_destination,new_directory)
+        new_directory = directory_to_copy.replace(self.root_path, "")
+        new_directory = os.path.join(directory_destination, new_directory)
         self.create_folder_if_not_exists(new_directory)
         return new_directory
 
     def detect_file_date(self, directory, filename):
         final_name = ""
         dest_directory = ""
-        possible_pattern = ["[January|February|March|April|May|June|July|August|September|October|November|December]*_[0-9]{2}__[0-9]*",
-                            "([0-9]{4})([0-9]{2})([0-9]{2})[-_]([0-9]*)",#Année#mois#jour#serie
-                            "([0-9]{2})([0-9]{2})([0-9]{2})[-_]([0-9]*)",#mois#Année#jour#serie
-                            "([0-9]{4})[-_]([0-9]{2})[-_]([0-9]{2})[\-\s]([0-9]{2})[h\:\-\s\.]([0-9]{2})[m\:\-\s\.]([0-9]{2})",#année mois jour heure minutes secondes
-                            "([0-9]{2})[-_]([0-9]{2})[-_]([0-9]{2})[\-\s]([0-9]{2})[h\:\-\s\.]([0-9]{2})[m\:\-\s\.]([0-9]{2})",#année mois jour heure minutes secondes
-                            ]
+        possible_pattern = [
+            # Année#mois#jour#serie
+            "[January|February|March|April|May|June|July|August|September|October|November|December]*_[0-9]{2}__[0-9]*",
+            # mois#Année#jour#serie
+            "([0-9]{4})([0-9]{2})([0-9]{2})[-_]([0-9]*)",
+            # année mois jour heure minutes secondes
+            "([0-9]{2})([0-9]{2})([0-9]{2})[-_]([0-9]*)",
+            # année mois jour heure minutes secondes
+            "([0-9]{4})[-_]([0-9]{2})[-_]([0-9]{2})[\-\s]([0-9]{2})[h\:\-\s\.]([0-9]{2})[m\:\-\s\.]([0-9]{2})",
+            "([0-9]{2})[-_]([0-9]{2})[-_]([0-9]{2})[\-\s]([0-9]{2})[h\:\-\s\.]([0-9]{2})[m\:\-\s\.]([0-9]{2})",
+        ]
         for pattern in possible_pattern:
-            matches = re.match(pattern,final_name)
-            if(matches != None):
+            matches = re.match(pattern, final_name)
+            if (matches != None):
                 print(matches.groups())
-        #Last chance : get filesystem creation date
+        # Last chance : get filesystem creation date
         if final_name == "":
             match = time.gmtime(os.path.getmtime(os.path.join(directory, filename)))
             final_name = repr(match[0]) + ":" + repr(match[1]) + ":" + repr(match[2]) + "_" + repr(
-                        match[3]) + ":" + repr(match[4]) + ":" + repr(match[5])
-            dest_directory = self.create_folder_if_not_exists(os.path.join(self.processed_folder, "emptyExif",repr(match[0]),repr(match[1]),repr(match[2])))
-        return final_name,dest_directory
+                    match[3]) + ":" + repr(match[4]) + ":" + repr(match[5])
+            dest_directory = self.create_folder_if_not_exists(
+                    os.path.join(self.processed_folder, "emptyExif", repr(match[0]), repr(match[1]), repr(match[2])))
+        return final_name, dest_directory
